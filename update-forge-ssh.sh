b8ceabcbe9bc5e7b066216bf9fd9e86ece6f8bac
bakerjo <bakerjo@me.com>
2025-12-18 22:59:20 +0300

    fix: remove missing v6 IPs

--- update-forge-ssh.sh
+++ update-forge-ssh.sh
@@ -4,7 +4,6 @@
 
 # Configuration
 FORGE_URL_V4="https://forge.laravel.com/ips-v4.txt"
-FORGE_URL_V6="https://forge.laravel.com/ips-v6.txt"
 SSH_PORT=22
 
 # Check if running as root
@@ -23,22 +22,10 @@ if [ $? -ne 0 ]; then
     exit 1
 fi
 
-IPS_V6=$(curl -s --fail $FORGE_URL_V6)
-if [ $? -ne 0 ]; then
-    echo "Error: Failed to fetch IPv6 list. Aborting to prevent lockout."
-    exit 1
-fi
-
 # 2. Allow IPv4
 echo "Updating IPv4 Rules..."
 for ip in $IPS_V4; do
     # 'limit' is often safer for SSH, but 'allow' is standard for trusted automated IPs
-    ufw allow from "$ip" to any port "$SSH_PORT" proto tcp comment 'Laravel Forge SSH'
-done
-
-# 3. Allow IPv6
-echo "Updating IPv6 Rules..."
-for ip in $IPS_V6; do
     ufw allow from "$ip" to any port "$SSH_PORT" proto tcp comment 'Laravel Forge SSH'
 done
 
